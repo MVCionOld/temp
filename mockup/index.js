e1671b115175412e921d825baa9d1510a6fd36cb
MVCionOld <tsionmichel1999@gmail.com>
2018-09-02 06:05:44 +0300

    WIF

--- mockup/index.js
+++ mockup/index.js
@@ -1,20 +1,31 @@
-let app = new Vue({
-    el: "#app",
-    data: {
-        message: "Hello, World!"
-    }
+let menuButton = new Vue({
+    el: "#menuButton",
+    data: {}
 });
 
-let tableBodyTags = new View({
+let userShortcut = new Vue({
+    el: "#userShortcut",
+    data: {}
+});
+
+let tableBodyTags = new Vue({
     el: "#tableBodyTags",
     data: {
-
+        rows: [
+            {id:1, theme:"Python", totalViews: 12453},
+            {id:2, theme:"Django", totalViews: 9084},
+            {id:3, theme:"TensorFlow", totalViews: 8173}
+        ]
     }
 });
 
-let tableBodyMembers = new View({
+let tableBodyMembers = new Vue({
     el: "#tableBodyMembers",
     data: {
-
+        rows: [
+            {id:1, nickname: "lexani42", contributions: 241},
+            {id:2, nickname: "Rivera", contributions: 180},
+            {id:3, nickname: "ChipX", contributions: 173}
+        ]
     }
 });
